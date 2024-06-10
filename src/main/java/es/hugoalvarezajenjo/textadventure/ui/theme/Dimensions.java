0ee1ee3704ccd650341e78e64912c38abe5b125e
Hugo Alvarez Ajenjo <hugo.alvarezajenjo@gmail.com>
2024-06-11 00:36:07 +0200

    docs: Create gh-pages Documentation

--- src/main/java/es/hugoalvarezajenjo/textadventure/ui/theme/Dimensions.java
+++
@@ -1,7 +0,0 @@
-package es.hugoalvarezajenjo.textadventure.ui.theme;
-
-import java.awt.Dimension;
-
-public final class Dimensions {
-    public static final Dimension DEFAULT_WINDOW_SIZE = new Dimension(800, 600);
-}
